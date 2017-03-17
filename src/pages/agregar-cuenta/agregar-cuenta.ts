43d71b67247604c9b7b4bd72ec8ec365421daefb
Luis Mariano Luporini <lluporini@blitzit.com.ar>
2017-03-17 00:01:12 -0300

    Archivos faltantes cuenta empresa y signup

---
+++ src/pages/agregar-cuenta/agregar-cuenta.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { NavController } from 'ionic-angular';
+
+import { TabsPage } from '../tabs/tabs';
+import { UserData } from '../../providers/user-data';
+
+
+@Component({
+  selector: 'page-user',
+  templateUrl: 'agregar-cuenta.html'
+})
+export class AgregarCuentaPage {
+  signup: {name?: string, email?: string, email2?: string, password?: string, password2?: string} = {};
+  submitted = false;
+
+  constructor(public navCtrl: NavController, public userData: UserData) {}
+
+  onSignup(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.userData.signup(this.signup).subscribe((signupSuccess: boolean) => {
+	if (signupSuccess) {
+	  this.navCtrl.push(TabsPage);
+	} else {
+	  // TODO
+	}
+      });
+    }
+  }
+}
