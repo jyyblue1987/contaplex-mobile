0699a59d80496091306d2a21cfcabc1f114a77eb
jyyblue <jyyblue1987@outlook.com>
2017-04-05 15:33:47 +0800

    [fixed] show error message when login return 403, error.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
@@ -16,23 +16,48 @@ export class LoginPage {
   login: {username?: string, password?: string} = {};
   submitted = false;
 
-  constructor(public navCtrl: NavController, public userData: UserData) { }
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public userData: UserData) { }
 
   onLogin(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
       this.userData.login(this.login.username, this.login.password).subscribe((loginSuccess: boolean) => {
-	if (loginSuccess) {
-	  this.navCtrl.push(TabsPage);
-	} else {
-	  // TODO
-	}
-      });
+      	if (loginSuccess) {
+      	  this.navCtrl.push(TabsPage);
+      	} else {
+      	  // TODO
+      	}
+      }, (err: any) => {
+            console.log(err);
+            this.showAlert(err.status);
+        });  
     }
   }
 
   onSignup() {
     this.navCtrl.push(SignupPage);
+  }
+
+  showAlert(status: number) {
+      var text = "En este momento no es posible establecer comunicación con ContaPlex. Por favor, intente nuevamente mas tarde";
+
+      if( status == 403 )
+        text = "El usuario y/o contraseña ingresados no son correctos. Verifique la información e intente nuevamente";
+
+      let alert = this.alertCtrl.create({
+        title: 'Login',
+        subTitle: text,
+        buttons: [            
+            {
+                text: 'OK',
+                handler: (data: any) => {
+                  this.login.password = '';
+                }
+            }
+          ]
+      });
+
+      alert.present();
   }
 }
